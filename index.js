7d55f0a13e674a2c4dc66ac948afb131dec137e3
Yago Fogaça <yagofogaca20@gmail.com>
2022-06-02 17:56:16 -0300

    Update01

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ console.clear();
 //
 
 programa: while (true) {
+    console.log('Bem vindo(a) ao JOKENPÔ !');
     const opcao = ['pedra', 'papel', 'tesoura'];
-    let opcaoComputadorPartida =
-        opcao[Math.floor(Math.random() * opcao.length)];
     let opcaoUsuarioPartida;
     let pontosUsuario = 0;
     let pontosComputador = 0;
@@ -15,11 +14,12 @@ programa: while (true) {
     while (isNaN(rodadas) == true) {
         rodadas = +prompt('Quantas rodadas deseja jogar? ');
     }
-    console.log();
     for (let i = 0; i < rodadas; i++) {
+        let opcaoComputadorPartida =
+            opcao[Math.floor(Math.random() * opcao.length)];
         console.log(`Escolha somente um número de 1 a 3!`);
         let opcaoUsuario = +prompt(
-            'Você escolhe qual? 1- pedra 2- papel 3- tesoura ',
+            'Você escolhe qual? 1-PEDRA | 2-PAPEL | 3-TESOURA ',
         );
         while (
             isNaN(opcaoUsuario) == true ||
@@ -42,11 +42,11 @@ programa: while (true) {
                     empates++;
                     break;
                 case 'pedra':
-                    console.log(`Papel ganha de pedra. Você ganhou!`);
+                    console.log(`Usuario ganhou!`);
                     pontosUsuario++;
                     break;
                 case 'tesoura':
-                    console.log(`Papel perde para tesoura. Você perdeu!`);
+                    console.log(`Computador ganhou!`);
                     pontosComputador++;
                     break;
             }
@@ -57,11 +57,11 @@ programa: while (true) {
                     empates++;
                     break;
                 case 'tesoura':
-                    console.log(`Pedra ganha de tesoura. Você ganhou!`);
+                    console.log(`Usuario ganhou!`);
                     pontosUsuario++;
                     break;
                 case 'papel':
-                    console.log(`Pedra perde para papel. Você perdeu!`);
+                    console.log(`Computador ganhou!`);
                     pontosComputador++;
                     break;
             }
@@ -72,37 +72,37 @@ programa: while (true) {
                     empates++;
                     break;
                 case 'papel':
-                    console.log(`Tesoura ganha de papel. Você ganhou!`);
+                    console.log(`Usuario ganhou!`);
                     pontosUsuario++;
                     break;
                 case 'pedra':
-                    console.log(`Tesoura perde para pedra. Você perdeu!`);
+                    console.log(`Computador ganhou!`);
                     pontosComputador++;
                     break;
             }
         }
     }
-    if (pontosUsuario > pontosComputador) {
+    if (empates > (pontosComputador || pontosUsuario)) {
         console.log();
-        console.log(`O placar ficou: 
+        console.log(`O placar final depois de ${rodadas} rodadas:
+                Usuario: ${pontosUsuario}
+                Computador: ${pontosComputador}
+                Empates: ${empates}
+                Que loucura, deu empate!`);
+    } else if (pontosUsuario > pontosComputador) {
+        console.log();
+        console.log(`O placar final depois de ${rodadas} rodadas:
                 Usuario: ${pontosUsuario}
                 Computador: ${pontosComputador}
                 Empates: ${empates}
                 O usuario foi o grande campeão!`);
     } else if (pontosUsuario < pontosComputador) {
         console.log();
-        console.log(`O placar ficou: 
+        console.log(`O placar final depois de ${rodadas} rodadas:
                 Usuario: ${pontosUsuario}
                 Computador: ${pontosComputador}
                 Empates: ${empates}
                 O computador foi o grande campeão!`);
-    } else {
-        console.log();
-        console.log(`O placar ficou: 
-                Usuario: ${pontosUsuario}
-                Computador: ${pontosComputador}
-                Empates: ${empates}
-                Que loucura, deu empate!`);
     }
     let jogarDeNovo = prompt('Deseja jogar novamente? ');
     while (jogarDeNovo != 'sim' && jogarDeNovo != 'nao') {
